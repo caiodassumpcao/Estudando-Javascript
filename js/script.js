10c248f29a1b0fdf6ab182e8d197f2701704739f
caiodassumpcao <120528960+caiodassumpcao@users.noreply.github.com>
2023-01-23 20:12:33 -0300

    Teste script
    Testando e adicionando scipt

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var mensagem = "oi";
+mensagem = "oi caio";
+
+alert(mensagem);
+
+function imprimeOla() {
+  console.log("Olá");
+}
+
+imprimeOla();
+
+function soma(op1, op2) {
+  return op1 + op2;
+}
